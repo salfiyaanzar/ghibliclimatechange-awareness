227c0e5909a6ff0271342b2a81cfaca37ec4b026
salfiyaanzar <salfiyaanzar@gmail.com>
2025-05-02 18:29:27 +0530

    port issues for deployment

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+console.log('PORT:', PORT);
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
-  console.log('PORT:', process.env.PORT);
 });
